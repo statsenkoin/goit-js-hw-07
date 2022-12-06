ba3006bb5dbfae08d8264898bcf36b0ac2cce757
Ihor Statsenko <statsenkoin@gmail.com>
2022-12-06 20:31:01 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+console.log(gallery);
+
+createMarkUp();
+
+function createMarkUp() {
+  const markup = galleryItems.reduce(
+    (acc, { preview, original, description }) => {
+      acc += `<div class="gallery__item">
+                  <a class="gallery__link" href="${original}">
+                    <img
+                      class="gallery__image"
+                      src="${preview}"
+                      data-source="${original}"
+                      alt="${description}"
+                    />
+                  </a>
+                </div>`;
+      return acc;
+    },
+    ''
+  );
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
