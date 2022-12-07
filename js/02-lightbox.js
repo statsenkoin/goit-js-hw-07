0e68f7f10cd98067cdbb9a32ee07b7a11325e364
Ihor Statsenko <statsenkoin@gmail.com>
2022-12-07 23:03:22 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+createMarkUp();
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  close: true,
+});
+
+function createMarkUp() {
+  const markup = galleryItems.reduce(
+    (acc, { preview, original, description }) => {
+      acc += `<a class="gallery__item" href="${original}">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  alt="${description}"
+                />
+              </a>`;
+      return acc;
+    },
+    ''
+  );
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
